08cce2a3fd38439dea8f934ca9c3699fabf86b2b
Nate Donato <natedonato@gmail.com>
2019-08-01 12:56:34 -0700

    updated band name generation

--- bandnames.js
+++ bandnames.js
@@ -1,31 +1,67 @@
 
 
 var adjectives;
+var nouns;
 
-fetch("https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&includePartOfSpeech=adjective&maxCorpusCount=-1&minDictionaryCount=0&maxDictionaryCount=-1&minLength=4&maxLength=-1&limit=100&api_key=nv78jxoqw7bu1xbgfx2tb3fij73km474ft9vydfozdmckawvn")
+function fetchNames(type = "adjective") {
+fetch(`https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&includePartOfSpeech=${type}&maxCorpusCount=-1&minDictionaryCount=0&maxDictionaryCount=-1&minLength=4&maxLength=-1&limit=100&api_key=nv78jxoqw7bu1xbgfx2tb3fij73km474ft9vydfozdmckawvn`)
     .then(function (response) {
         json = response.json();
         return json;
         }).then(function (myJson) {
-            adjectives = myJson;
+            if(type === "adjective"){
+                adjectives = myJson;
+                fetchNames("noun");
+            }else{
+                nouns = myJson;
+                initialize();
+            }
             console.log(myJson.length);
-            initialize();
-    });
 
+            
+    });
+}
+
+
+fetchNames();
 function initialize() {
 
     const content = document.getElementById("content");
     content.innerText= "Ready!";
     const button = document.createElement("button");
+
+    const name = document.createElement("p");
     button.innerText = "Get New Name";
     button.addEventListener("click", generateName)
     content.appendChild(button);
+    
+    content.appendChild(name);
 
 
+    let i = 0;
 
+    function getWord(array) {
+        let word1 = array[i].word.slice();
+        i += 1;
 
-    function generateName() {
-        console.log(adjectives[0].word);
+        if(adjectives.length <= i || nouns.length <= i){
+            fetchNames();
+            i = 0;
+        }
+
+        return word1;
+    }
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    function generateName(){
+        let word1 = capitalizeFirstLetter(getWord(adjectives));
+        let word2 = capitalizeFirstLetter(getWord(nouns));
+        if(word2[word2.length-1] !== "s"){word2 += "s";}
+
+        name.innerHTML = "The " + word1 +" "+ word2;
+
     }
 }
 
